5a71b36c4cc98c376e3db0a9b1d0cacdf4b12164
Daniel Erzse <derzse@gmail.com>
2023-03-19 11:48:58 +0200

    Update exemplu_clase.py

--- exemplu_clase.py
+++ exemplu_clase.py
@@ -2,8 +2,11 @@ import random
 
 nume = ["Ionescu", "Munteanu", "Toth", "Kiss", "Schmidt", "Smith"]
 prenume = ["Ion", "Andrei", "David", "Vlad", "Robert", "Mihai"]
+varste = range(13, 19, 1)
 clase = [7,8,9,10,11,12]
 scoli = ["Scoala Balcescu", "Scoala Dacia", "Scoala Oltea Doamna", "Liceul Emanuil Gojdu", "Liceul Mihai Eminscu", "Scoala Generala 16"]
+
+discipline = ["romana", "matematica", "engleza", "fizica", "informatica"]
 
 elevi =[]
 
@@ -16,7 +19,7 @@ class Elev:
         self.catalog = Catalog()
 
     def identificare(self):
-        print("Sunt {}, elev in clasa {} la {}".format(self.nume, self.clasa, self.scoala))
+        return "{}, {} ani, in clasa {} la {}".format(self.nume, self.varsta, self.clasa, self.scoala)
 
     def getNume(self):
         return self.nume
@@ -51,9 +54,31 @@ class Catalog:
         self.fizica = []
         self.informatica = []    
 
+    def addRomana(self, nota):
+        self.romana.append(nota)
+
+    def addMatematica(self, nota):
+        self.matematica.append(nota)
+
+    def addFizica(self, nota):
+        self.fizica.append(nota)
+
+    def addEngleza(self, nota):
+        self.engleza.append(nota)
+
+    def addInfo(self, nota):
+        self.informatica.append(nota)
+
 for i in range(30):
     elevi.append(Elev())
     elevi[i].setNume("{} {}".format(random.choice(nume), random.choice(prenume)))
+    elevi[i].setVarsta(random.choice(varste))
+    elevi[i].setClasa(random.choice(clase))
+    elevi[i].setScoala(random.choice(scoli))
 
 for i in range(30):
-    print(elevi[i].getNume())
+    for j in range(random.randrange(3,7)):
+        elevi[i].catalog.addRomana(random.randrange(4,11))
+
+for i in range(30):
+    print(elevi[i].catalog.romana)
